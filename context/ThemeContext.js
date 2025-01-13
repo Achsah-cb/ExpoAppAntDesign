41f6fa66ed63957fff0f18fa66415c7e595e1b82
Achsah-cb <achsah.codersboutique@gmail.com>
2025-01-13 11:57:51 +0530

    themecontext updated

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -8,10 +8,11 @@ export const ThemeProvider = ({ children }) => {
   const theme = {
     colors: {
       background: '#D9D9D9', // background light pink color
-      text: '#FFFFFF',
+      text: '#FFFFFF',  // white text color
       primary: '#FF2A64', // pink color
-      gold: '#E8B006',
-      cyan:'#E8B006'
+      gold: '#E8B006', // gold color for few words in a screen
+      cyan:'#E8B006'  // cyan color for few words in a screen
+      // You can add other colors as well here, following the above pattern
     },
     fontsize: {
       smaller: 10,
